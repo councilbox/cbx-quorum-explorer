72ad551410779eea0c1b7f55b2d0853d72437406
Aaron Fuentes <aaron.fuentes.cocodin@gmail.com>
2018-11-15 17:12:24 +0100

    Added hex to ascii conversion

--- webapp/src/containers/TablePage.js
+++ webapp/src/containers/TablePage.js
@@ -54,8 +54,15 @@ class TablePage extends React.Component {
 
     loadMore = async () => {
         let endpoint = API_TRANSACTIONS_ENDPOINT;
+
         if(this.props.location.pathname.includes('blocks')){
             endpoint = API_BLOCKS_ENDPOINT;
+        } else {
+            if (this.props.history.location.pathname.includes("block")) {
+                endpoint = `${endpoint}?block=${this.props.match.params.value}`
+            } else if (this.props.history.location.pathname.includes("account")) {
+                endpoint = `${endpoint}?address=${this.props.match.params.value}`
+            }
         }
         const response = await fetch(`${endpoint}?from=${this.state.data[this.state.data.length - 1].number}&limit=${LIMIT}`);
         const json = await response.json();
--- webapp/src/displayComponents/DataRow.js
+++ webapp/src/displayComponents/DataRow.js
@@ -3,116 +3,148 @@ import DataLink from '../displayComponents/DataLink';
 import Grid from './Grid';
 import GridItem from './GridItem';
 import LogRow from './LogRow';
+import { isEdge } from 'react-device-detect';
+import web3 from 'web3';
+import { Tooltip } from 'material-ui';
 
-const DataRow = props =>  {
-    const _renderValue = () => {
-        const { value } = props;
-        if (props.valueKey && value !== null) {
-            const key = props.valueKey;
-            if (key === "transactions") {
-                if (props.location.pathname.includes("block")) {
-                    return props.value === 0? '0 transactions' :
-                        <span>
-                            <DataLink
-                                valueId={props.valueId}
-                                history={props.history}
-                                type='blockTransactions'
-                            >
-                                {props.value}
-                            </DataLink> {`transaction${props.value > 1? 's' : ''}`}
-                        </span>
-                } else {
-                    return props.value === 0? '0 transactions' :
-                        <span>
-                            <DataLink
-                                valueId={props.valueId}
-                                history={props.history}
-                                type='accountTransactions'
-                            >
-                                {props.value}
-                            </DataLink> {`transaction${props.value > 1? 's' : ''}`}
-                        </span>;
-                }
-            }
+class DataRow extends React.Component {
 
-            if (key === "blockNumber" || key === "blockHash" ||  key === "parentHash") {
-                return <DataLink
-                    history={props.history}
-                    type='block' >
-                    {props.value}
-                </DataLink>
-            }
-            if(key === "to" || key === "from" || key === "address") {
-                return <DataLink
-                    history={props.history}
-                    type='account' >
-                    {props.value}
-                </DataLink>
-            }
-
-            if(key === "contractAddress") {
-                return (
-                    <span>
-                        Contract <DataLink
-                        history={props.history}
-                        type='account' >
-                        {props.value}
-                        </DataLink> created
-                    </span>
-                )
-            }
-
-            if (key === "transactionHash" || key === "creationTransaction") {
-                return <DataLink
-                    history={props.history}
-                    type='transaction' >
-                    {props.value}
-                </DataLink>
-            }
-
-            if(key === 'logs'){
-                if(!value){
-                    return '-';
-                }
-
-                if(Array.isArray(value) && value.length === 0){
-                    return '-';
-                }
-
-                return (
-                    <React.Fragment>
-                        {value.map((log, index) => (
-                            <LogRow key={`log_${index}`} log={log} index={index} />
-                        ))}
-                    </React.Fragment>
-                )
-            }
-
-            if (key === 'committedSeals' || key === 'validators'){
-                if(!Array.isArray(value)){
-                    let processedValue = value.slice(1);
-                    processedValue = processedValue.slice(0, -1);
-                    const items = processedValue.split(',');
-                    return items.map((item, index) => <div key={`${key}_${index}`} style={{marginTop: '0.3em'}}>{`${item.replace(/\'/g, '')}`}</div>);
-                }
-                return value.map((item, index) => <div key={`${key}_${index}`} style={{marginTop: '0.3em'}}>{`${item.replace(/\'/g, '')}`}</div>);
-            }
-
-            if(Array.isArray(value)){
-                if(value.length === 0){
-                    return '-';
-                }
-            }
-            return value;
-        }
+    state = {
+        ascii: false
     }
 
-    return (
-        <Grid style={{padding: '0.5em'}} className="hoverable">
-            <GridItem xs={3} md={2}><strong>{props.valueKey === 'contractAddress'? 'to' : props.valueKey}</strong>: </GridItem>
-            <GridItem style={{overflowWrap: "break-word"}} xs={12} md={10}>{_renderValue()}</GridItem>
-        </Grid>
-    )
+    toggleAscii = () => {
+        const ascii = this.state.ascii;
+
+        this.setState({
+            ascii: !ascii
+        });
+    }
+
+    render (){
+        const _renderValue = () => {
+            const { value } = this.props;
+            if (this.props.valueKey && value !== null) {
+                const key = this.props.valueKey;
+                if (key === "transactions") {
+                    if (this.props.location.pathname.includes("block")) {
+                        return this.props.value === 0? '0 transactions' :
+                            <span>
+                                <DataLink
+                                    valueId={this.props.valueId}
+                                    history={this.props.history}
+                                    type='blockTransactions'
+                                >
+                                    {this.props.value}
+                                </DataLink> {`transaction${this.props.value > 1? 's' : ''}`}
+                            </span>
+                    } else {
+                        return this.props.value === 0? '0 transactions' :
+                            <span>
+                                <DataLink
+                                    valueId={this.props.valueId}
+                                    history={this.props.history}
+                                    type='accountTransactions'
+                                >
+                                    {this.props.value}
+                                </DataLink> {`transaction${this.props.value > 1? 's' : ''}`}
+                            </span>;
+                    }
+                }
+
+                if (key === "blockNumber" || key === "blockHash" ||  key === "parentHash") {
+                    return <DataLink
+                        history={this.props.history}
+                        type='block' >
+                        {this.props.value}
+                    </DataLink>
+                }
+                if(key === "to" || key === "from" || key === "address") {
+                    return <DataLink
+                        history={this.props.history}
+                        type='account' >
+                        {this.props.value}
+                    </DataLink>
+                }
+
+                if(key === "contractAddress") {
+                    return (
+                        <span>
+                            Contract <DataLink
+                            history={this.props.history}
+                            type='account' >
+                            {this.props.value}
+                            </DataLink> created
+                        </span>
+                    )
+                }
+
+                if (key === "transactionHash" || key === "creationTransaction") {
+                    return <DataLink
+                        history={this.props.history}
+                        type='transaction' >
+                        {this.props.value}
+                    </DataLink>
+                }
+
+                if(key === 'logs'){
+                    if(!value){
+                        return '-';
+                    }
+
+                    if(Array.isArray(value) && value.length === 0){
+                        return '-';
+                    }
+
+                    return (
+                        <React.Fragment>
+                            {value.map((log, index) => (
+                                <LogRow key={`log_${index}`} log={log} index={index} />
+                            ))}
+                        </React.Fragment>
+                    )
+                }
+
+                if(key === 'input'){
+                    return (
+                        <Tooltip title={this.state.ascii? 'Click to convert to Hex' : 'Click to convert to ASCII'}>
+                            <div onClick={this.toggleAscii}>
+                                {this.state.ascii?
+                                    web3.utils.hexToAscii(value)
+                                :
+                                    value
+                                }
+                            </div>
+                        </Tooltip>
+                    )
+                }
+
+                if (key === 'committedSeals' || key === 'validators'){
+                    if(!Array.isArray(value)){
+                        let processedValue = value.slice(1);
+                        processedValue = processedValue.slice(0, -1);
+                        const items = processedValue.split(',');
+                        return items.map((item, index) => <div key={`${key}_${index}`} style={{marginTop: '0.3em'}}>{`${item.replace(/\'/g, '')}`}</div>);
+                    }
+                    return value.map((item, index) => <div key={`${key}_${index}`} style={{marginTop: '0.3em'}}>{`${item.replace(/\'/g, '')}`}</div>);
+                }
+
+                if(Array.isArray(value)){
+                    if(value.length === 0){
+                        return '-';
+                    }
+                }
+                return value;
+            }
+        }
+
+        return (
+            <Grid style={{padding: '0.5em'}} className="hoverable">
+                <GridItem xs={3} md={2}><strong>{this.props.valueKey === 'contractAddress'? 'to' : this.props.valueKey}</strong>: </GridItem>
+                <GridItem style={{overflowWrap: "break-word"}} xs={12} md={10}>{_renderValue()}</GridItem>
+            </Grid>
+        )
+    }
 }
 
 export default DataRow;
