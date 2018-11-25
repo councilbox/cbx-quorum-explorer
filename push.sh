876ca4a36b3722b1fc46a568d486bf2a079af069
Rodrigo Martínez <dev@brunneis.com>
2018-11-25 10:47:21 +0100

    Update push script

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source env.sh
 docker push councilbox/quorum-explorer-syncer
 docker push councilbox/quorum-explorer-http-api
-docker push councilbox/quorum-explorer-webapp
+docker push councilbox/quorum-explorer-webapp:$WEBAPP_VERSION
